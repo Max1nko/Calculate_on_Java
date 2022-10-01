90edeae0c95f5d62cd21d2804d6d737c062cc270
MarkPoncsik <114814659+MarkPoncsik@users.noreply.github.com>
2022-10-01 16:30:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
- class MyCalculate {
-     public static void main(String[] args) {
-         System.out.println("Mark не Bot")
+import java.util.Scanner;
 
-  
- class Mmain{
-     public ststic void main{
-         System.out.println("Максим БООтяра уууууу");
-     }
- }
+public class Main{
+    public static void main (String [] args){
+        System.out.println("введите число :");
+        Scanner inputone = new Scanner(System.in);
+        int input = inputone.nextInt();
+        int input1 = inputone.nextInt();
+        System.out.println(input + "+" + input1 + "=" + (input + input1) + "\n" + input + "-" + input1 + "=" + (input - input1) + "\n" + input1 + "-" + input + "=" + (input1 - input) + "\n" + input + "*" + input1 + "=" + (input * input1) + "\n" + input + ":" + input1 + "=" + (input / input1) + "\n" + input1 + ":" + input + "=" + (input1 / input)  );
+    }
+}
