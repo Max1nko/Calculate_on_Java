0821e242f02ffd9d2a96fa533f21584c95370ada
Max1nko <108717500+Max1nko@users.noreply.github.com>
2022-10-01 16:51:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,27 @@
-public interface MyIf {
-    int getNumber();
+public interface Moveable {
+    int RIGHT = 1;
+    int LEFT = 2;
+    int UP = 3;
+    int DOWN = 4;
 
-    static int staticMethod() {
-        return 0;
+    void moveRight();
+
+    void moveLeft();
+}
+public class Transport implements Moveable {
+    public void moveRight() {
+        System.out.println("Транспорт поворачивает вправо.");
+    }
+
+    public void moveLeft() {
+        System.out.println("Транспорт поворачивает влево.");
+    }
+
+    public void stop() {
+        System.out.println("Транспорт останавливается.");
+    }
+
+    public void start() {
+        System.out.println("Транспорт стартует.");
     }
 }
