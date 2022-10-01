c0d351e8a53c179d5c9003efecf3b7491cf52b4f
Max1nko <108717500+Max1nko@users.noreply.github.com>
2022-10-01 16:44:17 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,7 @@
-package ru.alexanderklimov.testapplication;
+public interface MyIf {
+    int getNumber();
 
-import ...
-
-public class MainActivity extends ActionBarActivity implements SubClass.MyCallback {
-
-    private TextView mResultTextView;
-    private SubClass mSubClass;
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        mResultTextView = (TextView)findViewById(R.id.textViewResult);
-
-        mSubClass = new SubClass();
-        mSubClass.registerCallBack(this);
+    static int staticMethod() {
+        return 0;
     }
-
-    public void onClick(View v) {
-        mSubClass.doSomething();
-    }
-
-    @Override
-    public void callBackReturn() {
-        mResultTextView.setText("Вызван метод обратного вызова");
-    }
-}    
+}
